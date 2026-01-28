f282f37b9598b1ec8866cc944a510e0e65345d4f
anonymuuss!! <marcosc3010@gmail.com>
2026-01-28 22:08:05 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,16 +6,16 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-
 const firebaseConfig = {
   apiKey: "AIzaSyDGxLmQDmohUivV1XxIsLIWAvDATLRROgE",
   authDomain: "fuego-en-la-isla.firebaseapp.com",
-  databaseURL: "https://fuego-en-la-isla-default-rtdb.firebaseio.com",
+  databaseURL: "https://fuego-en-la-isla-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "fuego-en-la-isla",
-  storageBucket: "fuego-en-la-isla.appspot.com",
+  storageBucket: "fuego-en-la-isla.firebasestorage.app",
   messagingSenderId: "837575806373",
   appId: "1:837575806373:web:d823ec3986cfee375cec4c"
 };
 
 // Inicializar Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
+export const app = initializeApp(firebaseConfig);
 
-export { auth, db };
+// Auth y Database
+export const auth = getAuth(app);
+export const db = getDatabase(app);
