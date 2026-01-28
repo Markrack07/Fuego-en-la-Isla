d031c9b98c6f5c321e808146bf6d04e044f862a5
anonymuuss!! <marcosc3010@gmail.com>
2026-01-28 21:41:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { auth, db } from './firebase-config.js';
+import { auth, db } from 'firebase-config.js';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 import { ref, set, onValue, push } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
 
