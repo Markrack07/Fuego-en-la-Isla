23d01c4bc7fe10a67fd1345fc0b4f776ab25a927
anonymuuss!! <marcosc3010@gmail.com>
2026-01-29 09:47:03 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,26 @@
-// Import Firebase
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js";
 
-// Configuración de Firebase
+// 🔥 Configuración Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDGxLmQDmohUivV1XxIsLIWAvDATLRROgE",
   authDomain: "fuego-en-la-isla.firebaseapp.com",
   projectId: "fuego-en-la-isla",
-  storageBucket: "fuego-en-la-isla.firebasestorage.app",
+  storageBucket: "fuego-en-la-isla.appspot.com",
   messagingSenderId: "837575806373",
   appId: "1:837575806373:web:d823ec3986cfee375cec4c"
 };
 
-// Inicializar Firebase
-const app = initializeApp(firebaseConfig);
+// 🔹 Inicializar app
+export const app = initializeApp(firebaseConfig);
 
-// Autenticación
+// 🔹 Auth
 export const auth = getAuth(app);
 
-// Realtime Database con URL específica de tu región
-export const db = getDatabase(app, "https://fuego-en-la-isla-default-rtdb.firebaseio.com");
+// 🔹 Realtime Database (URL IMPORTANTE)
+export const db = getDatabase(
+  app,
+  "https://fuego-en-la-isla-default-rtdb.europe-west1.firebasedatabase.app/"
+);
